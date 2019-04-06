78b42970536528668865ba7041983e176ac59ee2
adamtimpson <adamtimpson@gmail.com>
2019-04-06 21:46:20 +0100

    Started working on SetInterestsActivity

--- app/src/main/java/com/adamtimpson/mobilityaid/SetInterestsActivity.java
+++ app/src/main/java/com/adamtimpson/mobilityaid/SetInterestsActivity.java
@@ -2,12 +2,47 @@ package com.adamtimpson.mobilityaid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SetInterestsActivity extends AppCompatActivity {
+
+    private final String[] keys = NewRouteActivity.PLACES_KEYS;
+    private final String[] values = NewRouteActivity.PLACES_VALUES;
+
+    private ListView placesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_interests);
+
+        displayList(keys);
+
     }
+
+    private void displayList(String[] places) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, this.keys);
+
+        placesList = findViewById(R.id.interestPlacesList);
+        placesList.setAdapter(adapter);
+
+    }
+
+    private List<String> getKeysFromValues(List<String> values) {
+        List<String> keys = new ArrayList<String>();
+
+        for(String s: values) {
+            if(Arrays.asList(values).contains(s)) {
+                keys.add(Arrays.asList(this.keys).get(Arrays.asList(this.values).indexOf(s)));
+            }
+        }
+
+        return keys;
+    }
+
 }
